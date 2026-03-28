c75c18cb50179f48f5f75703f9aa6cc0086fc334
Shrichand Thapa <154520287+webmaster0109@users.noreply.github.com>
2026-03-29 01:25:22 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.chat_convo, name='chat'),
     path('chat/', views.chat_post, name='chat_post'),
-    path('chat/voice/', views.voice_to_text, name="voice"),
+    # path('chat/voice/', views.voice_to_text, name="voice"),
     path('chat/history/<str:session_id>/', views.chat_history_conversations, name='chat_history'),
     path('chat/sessions/', views.chat_sessions, name='chat_sessions'),
     path('chat/delete/<str:session_id>/', views.delete_session, name='delete_session'),
